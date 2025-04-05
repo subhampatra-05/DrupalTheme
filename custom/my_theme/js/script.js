c699cc076456798e335684c6be18b12fb7293509
subhampatra-05 <subhampatra0528@gmail.com>
2025-04-05 23:17:40 +0530

    added back to top buton

--- custom/my_theme/js/script.js
+++ custom/my_theme/js/script.js
@@ -1,1 +1,18 @@
 console.log("Theme JS is loaded.");
+// Back to Top Button
+window.onscroll = function () {
+    const btn = document.getElementById("backToTop");
+    if (btn) {
+      btn.style.display = window.scrollY > 300 ? "block" : "none";
+    }
+  };
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("backToTop");
+    if (btn) {
+      btn.onclick = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      };
+    }
+  });
+  
